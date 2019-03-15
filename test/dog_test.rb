09792afb93e9f57f1fecd48a2a8bff686c794c55
Amy Holt <amy.elizabeth.holt@gmail.com>
2019-03-15 09:45:41 -0600

    Update for small group activity

--- test/dog_test.rb
+++ test/dog_test.rb
@@ -5,18 +5,24 @@ class DogTest < Minitest::Test
 
   def test_it_exists
     sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
+
     assert_instance_of Dog, sodie
   end
 
   def test_it_has_attributes
+    skip
     sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
+
     assert_equal "Sodie", sodie.name
     assert_equal 9, sodie.age
     assert_equal "Shih-Tzu", sodie.breed
     assert_equal true, sodie.small_breed
+  end
 
+  def test_it_can_have_different_attributes
+    skip
     max = Dog.new("Max", 11, "Cocker Spaniel", false)
-    p max
+
     assert_equal "Max", max.name
     assert_equal 11, max.age
     assert_equal "Cocker Spaniel", max.breed
@@ -24,17 +30,31 @@ class DogTest < Minitest::Test
   end
 
   def test_it_starts_with_no_walks
+    skip
     sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
+
     assert_equal 0, sodie.walks
   end
 
-  def test_small_breeds_yap
+  def test_it_can_walk
+    skip
     sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
+    sodie.walk
+
+    assert_equal 1, sodie.walks
+  end
+
+  def test_small_breeds_yap
+    skip
+    sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
+
     assert_equal "Yap!", sodie.bark
   end
 
   def test_big_breed_roof
+    skip
     max = Dog.new("Max", 11, "Cocker Spaniel", false)
+
     assert_equal "ROOF", max.bark
   end
 
--- test/walker_test.rb
+++ test/walker_test.rb
@@ -5,26 +5,31 @@ require './lib/walker'
 class WalkerTest < Minitest::Test
 
   def test_it_exists
+    skip
     jeremiah = Walker.new("Jeremiah", 25)
     assert_instance_of Walker, jeremiah
   end
 
   def test_it_has_a_name
+    skip
     jeremiah = Walker.new("Jeremiah", 25)
     assert_equal "Jeremiah", jeremiah.name
   end
 
   def test_it_starts_with_poop_bags
+    skip
     jeremiah = Walker.new("Jeremiah", 25)
     assert_equal 25, jeremiah.poop_bags
   end
 
   def test_it_starts_with_no_clients
+    skip
     jeremiah = Walker.new("Jeremiah", 25)
     assert_equal [], jeremiah.clients
   end
 
   def test_it_can_accept_and_list_clients
+    skip
     jeremiah = Walker.new("Jeremiah", 25)
     sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
     max = Dog.new("Max", 11, "Cocker Spaniel", false)
@@ -37,6 +42,8 @@ class WalkerTest < Minitest::Test
   end
 
   def test_it_cannot_have_duplicate_clients
+    skip
+    # BONUS - come back to at end
     jeremiah = Walker.new("Jeremiah", 25)
     sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
     max = Dog.new("Max", 11, "Cocker Spaniel", false)
@@ -49,6 +56,7 @@ class WalkerTest < Minitest::Test
   end
 
   def test_it_can_walk_a_dog
+    skip
     jeremiah = Walker.new("Jeremiah", 25)
     sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
 
@@ -57,6 +65,7 @@ class WalkerTest < Minitest::Test
   end
 
   def test_when_it_walsks_dog_it_uses_poop_bag
+    skip
     jeremiah = Walker.new("Jeremiah", 25)
     sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
 
@@ -66,6 +75,7 @@ class WalkerTest < Minitest::Test
   end
 
   def test_it_can_list_all_puppy_clients
+    skip
     jeremiah = Walker.new("Jeremiah", 25)
     sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
     max = Dog.new("Max", 11, "Cocker Spaniel", false)
@@ -81,19 +91,68 @@ class WalkerTest < Minitest::Test
   end
 
   def test_it_can_list_all_elderly_clients
+    skip
+    jeremiah = Walker.new("Jeremiah", 25)
+    sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
+    max = Dog.new("Max", 11, "Cocker Spaniel", false)
+    oscar = Dog.new("Oscar", 1, "Shih-Tzu", true)
+    athena = Dog.new("Athena", 1, "German Shepard", false)
 
+    jeremiah.accept_client(sodie)
+    jeremiah.accept_client(max)
+    jeremiah.accept_client(oscar)
+    jeremiah.accept_client(athena)
+
+    assert_equal [max], jeremiah.elderly_clients
   end
 
   def test_it_can_list_all_client_names
+    skip
+    jeremiah = Walker.new("Jeremiah", 25)
+    sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
+    max = Dog.new("Max", 11, "Cocker Spaniel", false)
+    oscar = Dog.new("Oscar", 1, "Shih-Tzu", true)
+    athena = Dog.new("Athena", 1, "German Shepard", false)
 
+    jeremiah.accept_client(sodie)
+    jeremiah.accept_client(max)
+    jeremiah.accept_client(oscar)
+    jeremiah.accept_client(athena)
+
+    assert_equal ["Sodie", "Max", "Oscar", "Athena"], jeremiah.client_names
   end
 
   def test_it_can_list_names_ending_in_y_or_ie
+    skip
+    jeremiah = Walker.new("Jeremiah", 25)
+    sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
+    libby = Dog.new("Libby", 13, "Australian Shepard", false)
+    max = Dog.new("Max", 11, "Cocker Spaniel", false)
+    oscar = Dog.new("Oscar", 1, "Shih-Tzu", true)
 
+    jeremiah.accept_client(sodie)
+    jeremiah.accept_client(libby)
+    jeremiah.accept_client(max)
+    jeremiah.accept_client(oscar)
+
+    assert_equal [sodie, libby], jeremiah.name_ending_in_y_ie
   end
 
   def test_it_can_check_client_list_for_specific_breed
+    skip
+    jeremiah = Walker.new("Jeremiah", 25)
+    sodie = Dog.new("Sodie", 9, "Shih-Tzu", true)
+    max = Dog.new("Max", 11, "Cocker Spaniel", false)
+    oscar = Dog.new("Oscar", 1, "Shih-Tzu", true)
+    athena = Dog.new("Athena", 1, "German Shepard", false)
 
+    jeremiah.accept_client(sodie)
+    jeremiah.accept_client(max)
+    jeremiah.accept_client(oscar)
+    jeremiah.accept_client(athena)
+
+    assert jeremiah.breed?("Shih-Tzu")
+    refute jeremiah.breed?("Corgi")
   end
 
 end
